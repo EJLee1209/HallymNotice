c31d9444d01b7818a7aac784d2676939702be9e8
EJLee1209 <dldmswo1209@gmail.com>
2023-09-05 14:00:24 +0900

    Initial Commit

---
+++ HallymNotice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HallymNotice
+//
+//  Created by 이은재 on 2023/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
